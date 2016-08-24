1331925baca9633cb0e8743fd17d47cb380046a5
Jonathon Mascorella <joe_mascorella@me.com>
2016-08-24 21:27:49 +1000

    Updates to support boundary checking

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,33 +5,45 @@ from gopigo import set_left_speed, set_right_speed, set_speed, stop
 def waitForTarget():
     while (gpg.read_enc_status() != 0):
         sleep(0.1)
+        
+def checkTicks(aTicks):
+    if aTicks < 1 or aTicks > 500:
+        print("Please set ticks to more than one, or less than 500")
+        return False
+    return True
 
 def fwd(aTicks):
-    gpg.enc_tgt(1, 1, aTicks)
-    gpg.fwd()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(1, 1, aTicks)
+        gpg.fwd()
+        waitForTarget()
 
 def back(aTicks):
-    gpg.enc_tgt(1, 1, aTicks)
-    gpg.bwd()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(1, 1, aTicks)
+        gpg.bwd()
+        waitForTarget()
 
 def leftRot(aTicks):
-    gpg.enc_tgt(1, 1, aTicks)
-    gpg.left_rot()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(1, 1, aTicks)
+        gpg.left_rot()
+        waitForTarget()
 
 def left(aTicks):
-    gpg.enc_tgt(0, 1, aTicks)
-    gpg.left()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(0, 1, aTicks)
+        gpg.left()
+        waitForTarget()
 
 def rightRot(aTicks):
-    gpg.enc_tgt(1, 1, aTicks)
-    gpg.right_rot()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(1, 1, aTicks)
+        gpg.right_rot()
+        waitForTarget()
 
 def right(aTicks):
-    gpg.enc_tgt(1, 0, aTicks)
-    gpg.right()
-    waitForTarget()
+    if checkTicks(aTicks):
+        gpg.enc_tgt(1, 0, aTicks)
+        gpg.right()
+        waitForTarget()
